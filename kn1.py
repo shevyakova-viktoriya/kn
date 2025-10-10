9bcd2ce4b45d9e5834d2b09c012ddbacf371a20a
Viktoriya <viktoriasevakova621@gmail.com>
2025-10-11 01:43:19 +0300

    Главная функция

--- kn1.py
+++ kn1.py
@@ -31,3 +31,25 @@ def check_win(board):
             return board[each[0]]
     return False
 
+def main(board):
+    counter = 0
+    win = False
+    while not win:
+        draw_board(board)
+        if counter % 2 == 0:
+            take_input("X")
+        else:
+            take_input("O")
+        counter += 1
+
+        tmp = check_win(board)
+        if tmp:
+            print(tmp, "выиграл!")
+            win = True
+            break
+        if counter == 9:
+            print("Ничья!")
+            break
+    draw_board(board)
+
+
